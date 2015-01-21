2454bff0e3fe874720107191664001a716d68e84
franciscohui <git.francisco@gmail.com>
2015-01-21 08:43:19 -0500

    Guess the fact game
    Using label, buttons, and changing the text label with true, false, and
    reset buttons.

---
+++ File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Tap
+//
+//  Created by Francisco Hui on 1/21/15.
+//  Copyright (c) 2015 Francisco Hui. All rights reserved.
+//
+
+import Foundation
+
+//How do you open a file inside playground while Xcode?
--- Tap/ViewController.swift
+++ Tap/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var factLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func pressedShowFacts() {
+        //println("Hello")
+        factLabel.text = "Correct!"
+    }
+    @IBAction func pressedFalse() {
+        factLabel.text = "Sorry, it's a fact!"
+    }
+    @IBAction func pressedPlayAgain() {
+        factLabel.text = "In 1908, New York City passes the Sullivan Ordinance making it illegal for women to smoke in public."
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
